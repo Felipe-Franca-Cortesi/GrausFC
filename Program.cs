8d9002fec4ad7d350510b4c876aa7ac82d4d4fec
Felipe França <contatofelipefranca12@gmail.com>
2020-09-23 16:37:38 -0300

    Resolução do exercício GrausFC

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("------Converter temperatura-----");
+
+            Console.Write("Informe a temperatura em °F: ");
+            double f = Convert.ToDouble(Console.ReadLine());
+
+            double c = (f - 32) / 1.8;
+            Console.WriteLine($"°F = {f}");
+            Console.WriteLine($"{f}°F equivalem a {c}°C");
+            Console.WriteLine("Pressione qualquer tecla para sair");
+            Console.ReadKey();
+
+
+        }
+    }
+}
